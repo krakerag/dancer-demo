5c622d2f3cb66bfaff54b3ceaa5b6ed2609b5f2b
Matthew Hallsworth <matthew.hallsworth@gmail.com>
2023-11-17 00:52:33 +0000

    Updated the app to demonstrate how gitpod works for an editing flow.

--- app.pl
+++ app.pl
@@ -2,7 +2,7 @@
 use Dancer;
  
 get '/' => sub {
-    return "Dancer is responding inside Docker container";
+    return "Dancer is responding inside Docker container on a Gitpod instance in the cloud";
 };
  
 dance;
